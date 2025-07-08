539539bcff746afdaa11d76538053e513fb8462e
Fersa11 <fersa@gmx.net>
2025-07-08 16:25:34 +0200

    cors from "cors" added to backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import connectDB from "./config/db.js";
-
+import cors from "cors";
 // import dress from "./models/dress.model.js";
 import dressRoutes from "./routes/dress.route.js";
 
 const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: ["https://www.aegeansrosa.de", "https://aegeansrosa.de"],
+    credentials: true
+  })
+);
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
